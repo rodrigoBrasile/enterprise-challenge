f00b58afe36d470777477476601ff7946b277966
Rodrigo <rodrigosis@sidlar.ccom.br>
2023-05-31 21:17:06 -0300

    initiate commit

--- src/main/java/br/com/exaltasamba/enterprisechallenge/domain/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.exaltasamba.enterprisechallenge.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
-
-}
